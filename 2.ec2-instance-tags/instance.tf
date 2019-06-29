405dc2078c72098187fc649597498bdc032bb94b
aleti-pavan <pavan.hpsa@gmail.com>
2019-06-29 09:08:08 +0100

    with tags

---
+++ 2.ec2-instance-tags/instance.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIOGQ72E"
+    secret_key = "l3LrgQUQOWYVrCD"
+}
+
+resource "aws_instance" "ec2" {
+
+         ami = "ami-098bb5d92c8886ca1"
+         instance_type = "t2.micro"
+}
