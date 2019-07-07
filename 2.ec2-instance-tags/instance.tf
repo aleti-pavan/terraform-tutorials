4daa5519056dea21cf498194166fe5fb6f6d81e7
aleti-pavan <pavan.hpsa@gmail.com>
2019-07-07 09:19:00 +0100

    EC2 with Tags

--- 2.ec2-instance-tags/instance.tf
+++ 2.ec2-instance-tags/instance.tf
@@ -1,11 +1,10 @@
 provider "aws" {
-    region = "us-east-1"
-    access_key = "AKIOGQ72E"
-    secret_key = "l3LrgQUQOWYVrCD"
+  region     = "us-east-1"
+  access_key = "access_key"
+  secret_key = "secret_key"
 }
 
 resource "aws_instance" "ec2" {
-
-         ami = "ami-098bb5d92c8886ca1"
-         instance_type = "t2.micro"
+  ami           = "ami-098bb5d92c8886ca1"
+  instance_type = "t2.micro"
 }
