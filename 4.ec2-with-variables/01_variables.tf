1779d625f8876df4248b850906a565335f14be62
aleti-pavan <pavan.hpsa@gmail.com>
2019-07-07 09:21:47 +0100

    Terraform variables

--- 4.ec2-with-variables/01_variables.tf
+++ 4.ec2-with-variables/01_variables.tf
@@ -1,15 +1,15 @@
 variable "region" {
   description = "AWS region to create resources"
   default     = "us-east-1"
-  type = "string"
+  type        = "string"
 }
 
 variable "access_key" {
-  default = "AKIATF3X7GQ72E"
+  default = "access_key"
 }
 
 variable "secret_key" {
-  default = "l3LrgQDOOWYVrCD"
+  default = "secret_key"
 }
 
 variable "tags" {
@@ -17,10 +17,11 @@ variable "tags" {
 }
 
 variable "amis" {
-type = "map"
-default = {
-"us-west-1" = "ami-08949fb6466dd2cf3"
-"us-east-2" = "ami-05220ffa0e7fce3d1"
-"us-east-1" = "ami-098bb5d92c8886ca1"
-}
+  type = "map"
+
+  default = {
+    "us-west-1" = "ami-08949fb6466dd2cf3"
+    "us-east-2" = "ami-05220ffa0e7fce3d1"
+    "us-east-1" = "ami-098bb5d92c8886ca1"
+  }
 }
